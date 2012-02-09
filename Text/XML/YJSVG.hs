4f705ccb22732ec7af50206966edbe52387d12fc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2012-02-09 09:49:23 +0900

    new HaXml

--- Text/XML/YJSVG.hs
+++ Text/XML/YJSVG.hs
@@ -6,6 +6,7 @@ module Text.XML.YJSVG (
 
 import Text.XML.HaXml
 import Text.XML.HaXml.Pretty
+import Text.XML.HaXml.Types
 
 data SVG   = Line Double Double Double Double Color Double |
              Polyline [ ( Double, Double ) ] Color Color Double |
@@ -34,21 +35,21 @@ showSVG w h = show . document . svgToXml w h
 
 svgToElem :: SVG -> Element ()
 svgToElem (Line x1 y1 x2 y2 color lineWidth)
-  = Elem "line" [
-       ( "x1", AttValue [ Left $ show x1 ] )
-     , ( "y1", AttValue [ Left $ show y1 ] )
-     , ( "x2", AttValue [ Left $ show x2 ] )
-     , ( "y2", AttValue [ Left $ show y2 ] )
-     , ( "stroke", AttValue [ Left color ] )
-     , ( "stroke-width", AttValue [ Left $ show lineWidth ] )
+  = Elem (N "line") [
+       ( N "x1", AttValue [ Left $ show x1 ] )
+     , ( N "y1", AttValue [ Left $ show y1 ] )
+     , ( N "x2", AttValue [ Left $ show x2 ] )
+     , ( N "y2", AttValue [ Left $ show y2 ] )
+     , ( N "stroke", AttValue [ Left color ] )
+     , ( N "stroke-width", AttValue [ Left $ show lineWidth ] )
      ] []
 
 svgToElem (Polyline points fillColor lineColor lineWidth)
-  = Elem "polyline" [
-       ( "points", AttValue [ Left $ pointsToAttVal points ] )
-     , ( "fill"  , AttValue [ Left fillColor ] )
-     , ( "stroke", AttValue [ Left lineColor ] )
-     , ( "stroke-width", AttValue [ Left $ show lineWidth ] )
+  = Elem (N "polyline") [
+       ( N "points", AttValue [ Left $ pointsToAttVal points ] )
+     , ( N "fill"  , AttValue [ Left fillColor ] )
+     , ( N "stroke", AttValue [ Left lineColor ] )
+     , ( N "stroke-width", AttValue [ Left $ show lineWidth ] )
      ] []
   where
   pointsToAttVal :: [ ( Double, Double ) ] -> String
@@ -57,57 +58,57 @@ svgToElem (Polyline points fillColor lineColor lineWidth)
     = show x ++ "," ++ show y ++ " " ++ pointsToAttVal ps
 
 svgToElem (Rect x y w h sw cf cs)
-  = Elem "rect" [
-   ( "x", AttValue [ Left $ show x ] )
- , ( "y", AttValue [ Left $ show y ] )
- , ( "width", AttValue [ Left $ show w ] )
- , ( "height", AttValue [ Left $ show h ] )
- , ( "stroke-width", AttValue [ Left $ show sw ] )
- , ( "fill", AttValue [ Left cf ] )
- , ( "stroke", AttValue [ Left cs ] )
+  = Elem (N "rect") [
+   ( N "x", AttValue [ Left $ show x ] )
+ , ( N "y", AttValue [ Left $ show y ] )
+ , ( N "width", AttValue [ Left $ show w ] )
+ , ( N "height", AttValue [ Left $ show h ] )
+ , ( N "stroke-width", AttValue [ Left $ show sw ] )
+ , ( N "fill", AttValue [ Left cf ] )
+ , ( N "stroke", AttValue [ Left cs ] )
  ] []
 
 svgToElem (Text x y s c t)
-  = Elem "text" [
-   ( "x", AttValue [ Left $ show x ] )
- , ( "y", AttValue [ Left $ show y ] )
- , ( "font-size", AttValue [ Left $ show s ] )
- , ( "fill", AttValue [ Left c ] )
+  = Elem (N "text") [
+   ( N "x", AttValue [ Left $ show x ] )
+ , ( N "y", AttValue [ Left $ show y ] )
+ , ( N "font-size", AttValue [ Left $ show s ] )
+ , ( N "fill", AttValue [ Left c ] )
  ] [ CString False t () ]
 
 svgToElem (Circle x y r c)
-  = Elem "circle" [
-       ( "cx", AttValue [ Left $ show x ] )
-     , ( "cy", AttValue [ Left $ show y ] )
-     , ( "r", AttValue [ Left $ show r ] )
-     , ( "fill", AttValue [ Left c ] )
+  = Elem (N "circle") [
+       ( N "cx", AttValue [ Left $ show x ] )
+     , ( N "cy", AttValue [ Left $ show y ] )
+     , ( N "r", AttValue [ Left $ show r ] )
+     , ( N "fill", AttValue [ Left c ] )
      ] []
 
 svgToElem (Image x y w h p)
-  = Elem "image" [
-       ( "x", AttValue [ Left $ show x ] )
-     , ( "y", AttValue [ Left $ show y ] )
-     , ( "width", AttValue [ Left $ show w ] )
-     , ( "height", AttValue [ Left $ show h ] )
-     , ( "xlink:href", AttValue [ Left p ] )
+  = Elem (N "image") [
+       ( N "x", AttValue [ Left $ show x ] )
+     , ( N "y", AttValue [ Left $ show y ] )
+     , ( N "width", AttValue [ Left $ show w ] )
+     , ( N "height", AttValue [ Left $ show h ] )
+     , ( N "xlink:href", AttValue [ Left p ] )
      ] []
 
 svgToElem (Group trs svgs)
-  = Elem "g" (
+  = Elem (N "g") (
       map (\ tr -> let a = showTrans tr
-                    in ( "transform", AttValue [ Left $ a ] ) ) trs
+                    in ( N "transform", AttValue [ Left $ a ] ) ) trs
      ) $ map (flip CElem () . svgToElem) svgs
 
 
 svgToXml :: Double -> Double -> [ SVG ] -> Document ()
 svgToXml w h svgs
   = Document prlg ent
-      (Elem "svg" (emAtt w h) $ map (flip CElem () . svgToElem) svgs) els
+      (Elem (N "svg") (emAtt w h) $ map (flip CElem () . svgToElem) svgs) els
 
 pmsc    = [ Comment " MADE BY SVG.HS " ]
 pblit   = "-//W3C//DTD SVG 1.1//EN"
 sslit   = "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"
-doctype = DTD "svg" (Just (PUBLIC (PubidLiteral pblit) (SystemLiteral sslit))) []
+doctype = DTD (N "svg") (Just (PUBLIC (PubidLiteral pblit) (SystemLiteral sslit))) []
 xmldcl  = XMLDecl "1.0" (Just (EncodingDecl "UTF-8")) Nothing
 prlg    = Prolog (Just xmldcl) pmsc (Just doctype) []
 
@@ -117,25 +118,25 @@ xlink   = "http://www.w3.org/1999/xlink"
 width   = "400"
 height  = "640"
 emAtt  w h = [
-   ( "xmlns", AttValue [ Left xmlns ] )
- , ( "version", AttValue [ Left ver ] )
- , ( "xmlns:xlink", AttValue [ Left xlink ] )
- , ( "width", AttValue [ Left $ show w ] )
- , ( "height", AttValue [ Left $ show h ] )
+   ( N "xmlns", AttValue [ Left xmlns ] )
+ , ( N "version", AttValue [ Left ver ] )
+ , ( N "xmlns:xlink", AttValue [ Left xlink ] )
+ , ( N "width", AttValue [ Left $ show w ] )
+ , ( N "height", AttValue [ Left $ show h ] )
  ]
-em ougon= Elem "svg" (emAtt 100 200) [ CElem rect (), CElem (text ougon) () ]
-rect = Elem "rect" [
-   ( "x", AttValue [ Left "20" ] )
- , ( "y", AttValue [ Left "30" ] )
- , ( "width", AttValue [ Left "80" ] )
- , ( "height", AttValue [ Left "100" ] )
- , ( "fill", AttValue [ Left "red" ] )
- , ( "stroke", AttValue [ Left "none" ] )
+em ougon= Elem (N "svg") (emAtt 100 200) [ CElem rect (), CElem (text ougon) () ]
+rect = Elem (N "rect") [
+   ( N "x", AttValue [ Left "20" ] )
+ , ( N "y", AttValue [ Left "30" ] )
+ , ( N "width", AttValue [ Left "80" ] )
+ , ( N "height", AttValue [ Left "100" ] )
+ , ( N "fill", AttValue [ Left "red" ] )
+ , ( N "stroke", AttValue [ Left "none" ] )
  ] []
-text ougon = Elem "text" [
-   ( "x", AttValue [ Left "20" ] )
- , ( "y", AttValue [ Left "50" ] )
- , ( "font-size", AttValue [ Left "20" ] )
+text ougon = Elem (N "text") [
+   ( N "x", AttValue [ Left "20" ] )
+ , ( N "y", AttValue [ Left "50" ] )
+ , ( N "font-size", AttValue [ Left "20" ] )
  ] [ CString False ougon () ]
 
 ent     = []
